c15dc58366803db076b096aff8bf3dcf00a07250
Xir <prssergey@gmail.com>
2025-03-16 02:04:20 +0300

    feat: endless zen mode options

--- src/classes/MainMenu/EndlessZen.ts
+++ src/classes/MainMenu/EndlessZen.ts
@@ -1,30 +1,113 @@
 import { MenuTab } from "./MenuTab";
-import { ColorType, GameMode, MenuTabProps } from "../../types";
-import { ValueSelector } from "../ui/ValueSelector";
-import { cicleThrougColors, getColorName } from "../../utils";
-import { LevelEditor } from "./LevelEditor";
+import {
+  ColorType,
+  GameMode,
+  LevelDifficulty,
+  MenuTabProps,
+} from "../../types";
+
+import { OptionFolder } from "../ui/html/OptionFolder";
+import { ValueSelector } from "../ui/html/ValueSelector";
+import { generateLevel } from "../../utils";
+import { OptionSelector } from "../ui/html/OptionSelector";
 
 export class EndlessZen extends MenuTab {
   selectedFillColor: ColorType = 0;
   rows: number = 8;
   columns: number = 10;
+  difficulty: LevelDifficulty = "Medium";
+  colorsCount: number = 4;
 
   constructor(props: MenuTabProps) {
     super(props);
     const { scene, width, height } = props;
 
-    const { levelEditor } = scene.cache.json.get("config")["mainMenu"];
-  }
-  
-  private generateField(rows: number, cols: number, color: ColorType) {
-    const arr: number[][] = [];
-    for (let i = 0; i < rows; i++) {
-      arr[i] ??= [];
-      for (let j = 0; j < cols; j++) {
-        arr[i][j] = color;
-      }
-    }
+    const viewBox = scene.add
+      .dom(0, 0, "div", {
+        width: `${width}px`,
+        height: `${height}px`,
+      })
+      .setOrigin(0);
 
-    return arr;
+    const folder = new OptionFolder("Grid Options");
+    viewBox.node.appendChild(folder.container);
+
+    const columns = new ValueSelector(
+      "Columns",
+      this.columns,
+      () => {
+        this.columns = Phaser.Math.Clamp(this.columns - 1, 2, 100);
+        return this.columns;
+      },
+      () => {
+        this.columns = Phaser.Math.Clamp(this.columns + 1, 2, 100);
+        return this.columns;
+      }
+    );
+
+    const rows = new ValueSelector<number>(
+      "Rows",
+      this.rows,
+      () => {
+        this.rows = Phaser.Math.Clamp(this.rows - 1, 2, 100);
+        return this.rows;
+      },
+      () => {
+        this.rows = Phaser.Math.Clamp(this.rows + 1, 2, 100);
+        return this.rows;
+      }
+    );
+
+    const colorsCount = new ValueSelector<number>(
+      "Colors count",
+      this.colorsCount,
+      () => {
+        this.colorsCount = Phaser.Math.Clamp(this.colorsCount - 1, 2, 8);
+        return this.colorsCount;
+      },
+      () => {
+        this.colorsCount = Phaser.Math.Clamp(this.colorsCount + 1, 2, 8);
+        return this.colorsCount;
+      }
+    );
+
+    const diffOptions: LevelDifficulty[] = ["Easy", "Medium", "Hard", "Mess"];
+    const difficultyIndex = diffOptions.indexOf(this.difficulty);
+
+    const difficulty = new OptionSelector(
+      "Difficulty",
+      difficultyIndex,
+      diffOptions,
+      false,
+      (selectedIndex) => {
+        this.difficulty = diffOptions[selectedIndex];
+      }
+    );
+
+    folder.add(columns.container);
+    folder.add(rows.container);
+    folder.add(colorsCount.container);
+    folder.add(difficulty.container);
+
+    this.actionBtn.btn.setInteractive();
+    this.actionBtn.btn.on("pointerdown", () => {
+      this.scene.scene.start("LoadingGame", {
+        mode: GameMode.Endless,
+        endlessOptions: {
+          rows: this.rows,
+          columns: this.columns,
+          colorsCount: this.colorsCount,
+          difficulty: this.difficulty,
+        },
+        levelData: generateLevel(
+          this.rows,
+          this.columns,
+          this.colorsCount,
+          this.difficulty
+        ),
+      });
+    });
+
+    this.container.add([viewBox]);
   }
 }
